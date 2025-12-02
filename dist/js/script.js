356591e5a355eb6834b4e448ef46478467b68837
rafietaufikxpro-dot <rafietaufik.xpro@gmail.com>
2025-12-02 15:12:06 +0700

    pa update

--- dist/js/script.js
+++ dist/js/script.js
@@ -131,3 +131,13 @@ document.addEventListener("DOMContentLoaded", function () {
   // Inisialisasi ikon
   updateIcon();
 });
+// navbar
+// Navbar scroll effect
+window.addEventListener("scroll", function() {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {  // ketika scroll lebih dari 50px
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
